df158ccce34949a944cdabd9e5e02e8c76f4a6d8
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-09 19:56:33 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -49,10 +49,10 @@ HELP_STRINGS = """
 Hello! my name *{}*.
 
 *Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /settings:
+ - ✘start: start the bot
+ - ✘help: PM's you this message.
+ - ✘help <module name>: PM's you info about that module.
+ - ✘settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
 
