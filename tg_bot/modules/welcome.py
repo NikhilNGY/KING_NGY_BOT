f26f923e12546bdf6216b8279e20a77959bf1e80
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2020-07-18 18:06:29 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -93,7 +93,7 @@ def new_member(bot: Bot, update: Update, job_queue: JobQueue):
 
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("Oh, Genos? Let's get this moving.")
+                update.effective_message.reply_text("My Owner(Creator) Join This Group 😱😍😱.")
                 welcome_log = (f"{html.escape(chat.title)}\n"
                                f"#USER_JOINED\n"
                                f"Bot Owner just joined the chat")
